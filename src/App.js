374bca75a8a7b5267b0ca14815ca9614c3693851
hexoul <crosien@gmail.com>
2022-12-20 22:54:16 +0900

    Clone homepage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img
+        src="https://doda-static.com/img/600/65/1/1/1635922598145-0488440.png"
+        className="App-LogoImage"
+        alt="logo"
+      />
+      <div class="App-Title">나와 잘 맞는 크리스마스 캐롤은?</div>
+      <div class="App-Subtitle">
+        <b>유니버설 뮤직 코리아 x 트렌드어워드</b>
+        <br />
+        찰떡 같은 캐롤을 추천해드립니다!
+      </div>
+      <img
+        src="https://doda-static.com/img/900/80/1/1/1636617853966-0117112.jpg"
+        className="App-StartImage"
+        alt="start"
+      />
+      <button
+        className="App-StartButton"
+      >
+        테스트 시작하기
+      </button>
     </div>
   );
-}
+};
 
 export default App;
