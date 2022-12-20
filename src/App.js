eb8d9cee8e2f4d5bf262c78507989adbce3758ad
hexoul <crosien@gmail.com>
2022-12-21 00:23:17 +0900

    Refactor with MUI

--- src/App.js
+++ src/App.js
@@ -1,31 +1,8 @@
 import "./App.css";
-import { FaChevronRight } from "react-icons/fa";
+import Home from "./components/Home";
 
 const App = () => {
-  return (
-    <div className="App">
-      <img
-        src="https://doda-static.com/img/600/65/1/1/1635922598145-0488440.png"
-        className="App-LogoImage"
-        alt="logo"
-      />
-      <div class="App-Title">나와 잘 맞는 크리스마스 캐롤은?</div>
-      <div class="App-Subtitle">
-        <b>유니버설 뮤직 코리아 x 트렌드어워드</b>
-        <br />
-        찰떡 같은 캐롤을 추천해드립니다!
-      </div>
-      <img
-        src="https://doda-static.com/img/900/80/1/1/1636617853966-0117112.jpg"
-        className="App-StartImage"
-        alt="start"
-      />
-      <button className="App-StartButton">
-        <FaChevronRight className="App-StartButton__icon" />
-        테스트 시작하기
-      </button>
-    </div>
-  );
+  return <Home />;
 };
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,45 @@
+import "./Home.css";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import IconButton from "@mui/material/IconButton";
+
+const Home = () => {
+  return (
+    <div className="Home">
+      <img
+        src={`${process.env.PUBLIC_URL}/logo512.png`}
+        className="Home-LogoImage"
+        alt="logo"
+      />
+      <div class="Home-Title">나와 잘 맞는 크리스마스 캐롤은?</div>
+      <div class="Home-Subtitle">
+        <b>유니버설 뮤직 코리아 x 트렌드어워드</b>
+        <br />
+        찰떡 같은 캐롤을 추천해드립니다!
+      </div>
+      <img
+        src={`${process.env.PUBLIC_URL}/img/start-image.jpg`}
+        className="Home-StartImage"
+        alt="start"
+      />
+      <IconButton
+        className="Home-StartButton"
+        style={{
+          color: "black",
+          backgroundColor: "white",
+          borderRadius: "10px",
+          fontSize: "large",
+          fontFamily: "GangwonEdu_OTFBoldA",
+          boxShadow: "rgba(255, 255, 255, 0.34) 0px 5px 20px",
+        }}
+        onClick={() => {
+          alert("clicked");
+        }}
+      >
+        <ChevronRightIcon />
+        테스트 시작하기
+      </IconButton>
+    </div>
+  );
+};
+
+export default Home;
